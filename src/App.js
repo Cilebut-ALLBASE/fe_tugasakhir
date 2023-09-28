a8da0ac9d8ab4857975539752c055f5ef9ef3dde
valiantmk17 <iyan1708@gmail.com>
2023-09-28 13:22:21 +0700

    login as

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Router, Route } from 'react-router-dom';
 import { Login } from './page/login/login';
 import { Register } from './page/register/register';
+import { Start } from './page/start';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Login />}></Route>
         <Route path='/register' element={<Register />}></Route>
+        <Route path='/start' element={<Start />}></Route>
       </Routes>
     </div>
   );
