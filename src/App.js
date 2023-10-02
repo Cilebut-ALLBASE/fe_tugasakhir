e7912b3349542c87b63bd2f0fbb78c563c0df5b1
amelia azmi <azmiamelia24@gmail.com>
2023-10-02 14:25:39 +0700

    absen&sidebar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { Login } from './page/login/login';
 import { Register } from './page/register/register';
 import { Start } from './page/start';
 import Home from './page/home';
+import { Absen } from './page/absen/absen';
+import { Sidebar } from './page/sidebar/sidebar';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
@@ -13,6 +17,7 @@ function App() {
         <Route path='/register' element={<Register />}></Route>
         <Route path='/start' element={<Start />}></Route>
         <Route path='/' element={<Home />}></Route>
+        <Route path='/absen' element={<Absen />}></Route>
       </Routes>
     </div>
   );
---
+++ src/page/sidebar/sidebar.js
@@ -0,0 +1,49 @@
+import { useRef } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlaneUp, faUserCheck, faSquare } from '@fortawesome/free-solid-svg-icons';
+import '../../styles/sidebar-style.css';
+
+export function Sidebar() {
+	const navRef = useRef();
+
+	
+
+	return (
+		<header>
+			<h3>USER</h3>
+			<nav ref={navRef}>
+				<div className="plane">
+					<FontAwesomeIcon icon={faPlaneUp} />
+				</div>
+				<div className="user">
+					<FontAwesomeIcon icon={faUserCheck} />
+				</div>
+				<div className="square-1">
+					<FontAwesomeIcon icon={faSquare} />
+				</div>
+				<div className="square-2">
+					<FontAwesomeIcon icon={faSquare} />
+				</div>
+				<div className="square-3">
+					<FontAwesomeIcon icon={faSquare} />
+				</div>
+				<div className="square-4">
+					<FontAwesomeIcon icon={faSquare} />
+				</div>
+				<a className="ovr-text" href="./overview">Overview</a>
+				<br>
+				</br>
+				<a className="att-text" href="./absen">Attendance</a>  
+				<br>
+				</br>
+				<a className="lv-text" href="./leave">Leave</a>
+				<br>
+				</br>
+                
+			</nav>
+			
+		</header>
+	);
+}
+
+export default Sidebar;
