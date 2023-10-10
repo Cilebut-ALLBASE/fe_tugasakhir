a5b151b2932213a52c1f4cc957081d05822e50f8
FirmanMalik6 <firmanmalikaz@gmail.com>
2023-10-10 08:05:23 +0700

    p

--- src/components/absent-count.js
+++ src/components/absent-count.js
@@ -1,2 +1,2 @@
 // Jumlah orang yang absent
-// Menurut gw, ini isinya kodingan untuk mengetahui org yg udah absent, trus outputnya hanya angka berapa yg telah absent, nanti gw import di home dan home-hr
+// Menurut gw, ini isinya kodingan untuk mengetahui org yg absent, trus outputnya hanya angka berapa yg absent, nanti gw import di home dan home-hr
