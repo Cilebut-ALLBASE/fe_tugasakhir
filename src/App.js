5aea310ea4287ea82ab235ec3fe7425d14c03844
FirmanMalik6 <firmanmalikaz@gmail.com>
2023-10-12 21:13:36 +0700

    Terbaru 100% REALL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Routes, Router, Route } from 'react-router-dom';
 import { Login } from './page/login/login';
 import { Register } from './page/register/register';
-import { Start } from './page/start';
 import Home from './page/home';
 import { Absen } from './page/absen/absen';
 import { Leave } from './page/leave/leave';
@@ -16,7 +15,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Login />}></Route>
         <Route path='/register' element={<Register />}></Route>
-        <Route path='/start' element={<Start />}></Route>
         <Route path='/home' element={<Home />}></Route>
         <Route path='/absen' element={<Absen />}></Route>
         <Route path='/leave' element={<Leave />}></Route>
